9048b0cad176dfe7beb1215992f129f3f9a353ed
Matt Wagaman <averaqedev@gmail.com>
2022-01-26 16:05:57 -0800

    update dependencies

--- src/config.js
+++ src/config.js
@@ -3,7 +3,11 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   // Database Info
-  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/superwho',
+  DB_HOST: process.env.DB_HOST || 'localhost',
+  DB_PORT: process.env.DB_PORT || 5432,
+  DB_USER: process.env.DB_USER || 'postgres',
+  DB_PASS: process.env.DB_PASS || 'postgres',
+  DB_NAME: process.env.DB_NAME || 'superwho',
   // JWT Info
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   // SuperHeroAPI Info
